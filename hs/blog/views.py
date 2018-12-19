79cd5eb0d2a57f8cffa40b5d3b94fc4dd53a49bc
lostroom <mi.agarkov@gmail.com>
2018-12-19 16:19:24 +0300

    hs v 0.1

--- hs/blog/views.py
+++ hs/blog/views.py
@@ -3,15 +3,18 @@ from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 
 from django.db import connections
 from django.db.models import Count
 from django.http import JsonResponse
 from .models import Play
 
+@login_required
 def graph(request):
     return render(request, 'blog/graph.html')
 
+@login_required
 def play_count_by_month(request):
     data = Play.objects.all() \
         .extra(select={'month': connections[Play.objects.db].ops.date_trunc_sql('month', 'date')}) \
@@ -19,14 +22,17 @@ def play_count_by_month(request):
         .annotate(count_items=Count('id'))
     return JsonResponse(list(data), safe=False)
 
+@login_required
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
+@login_required
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
